faf479ae7bf6fa75b3e56bd1d5b4a25519804658
timint <info@tim-international.net>
2025-07-21 10:22:07 +0200

    * Replace 7zip-electron-min with 7zip-min

--- src/fork/util/Zip.ts
+++ src/fork/util/Zip.ts
@@ -1,50 +1,16 @@
-import { appendFile, existsSync, copyFile } from '@shared/fs-extra'
-import { join, basename } from 'node:path'
-import { createRequire } from 'node:module'
-import { pathFixedToUnix } from '@shared/utils'
+import { unpack } from '7zip-min'
 
-const require = createRequire(import.meta.url)
-const compressing = require('7zip-min-electron')
+export async function zipUnpack(src: string, dest: string): Promise<boolean> {
+  console.log('[zipUnpack] start:', {
+    source: src,
+    destination: dest
+  })
 
-export function zipUnpack(fp: string, dist: string) {
-  console.log('zipUnpack start: ', fp, dist, global.Server.Static!)
-  return new Promise(async (resolve, reject) => {
-    const info = {
-      fp,
-      dist,
-      static: global.Server.Static,
-      isIncludes: fp.includes(global.Server.Static!)
-    }
-    await appendFile(
-      join(global.Server.BaseDir!, 'debug.log'),
-      `[zipUnpack][info]: ${JSON.stringify(info, undefined, 4)}\n`
-    )
-    let file = fp
-    if (pathFixedToUnix(fp).includes(pathFixedToUnix(global.Server.Static!))) {
-      const cacheFP = join(global.Server.Cache!, basename(fp))
-      if (!existsSync(cacheFP)) {
-        try {
-          await copyFile(fp, cacheFP)
-        } catch (e) {
-          await appendFile(
-            join(global.Server.BaseDir!, 'debug.log'),
-            `[zipUnpack][copyFile][error]: ${e}\n`
-          )
-        }
-      }
-      file = cacheFP
-      console.log('cacheFP: ', fp)
-    }
-    compressing.unpack(file, dist, async (err: any, res: any) => {
-      console.log('zipUnpack end: ', err, res)
-      if (err) {
-        await appendFile(
-          join(global.Server.BaseDir!, 'debug.log'),
-          `[zipUnpack][unpack][error]: ${err}\n`
-        )
-        reject(err)
-        return
-      }
+  // Perform unpack operation
+  return new Promise((resolve, reject) => {
+    unpack(src, dest, async err => {
+      console.log('[zipUnpack] end:', err ? 'error' : 'success')
+      if (err) return reject(err)
       resolve(true)
     })
   })
